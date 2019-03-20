15264dc6c26dd0afdc3b51799880bd7ef446c374
Rohan Bassett <rohan@24digital.com.au>
2019-03-20 17:24:58 +1100

    &c.

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -2,4 +2,8 @@ for file in $(find ./src -name *.js);
 do
     echo $file
 	node ./node_modules/eslint/bin/eslint.js $file
+	# If the result of the previous command is NOT zero
+	if [ $? -ne 0 ]; then
+	 exit 1
+	fi
 done
