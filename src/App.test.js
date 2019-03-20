f045d3efc7e6a75fb04ab750bb39a896849012d1
Rohan Bassett <rohan@24digital.com.au>
2019-03-20 15:29:59 +1100

    unstyle

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,15 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
+it("renders without crashing", () => {const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 test("falsey mcfalseface", () => {
-  // ch-ch-ch-changes
-  expect(false).toBe(false);
+// ch-ch-ch-changes
+expect(false).toBe(false);
 });
 
-test("let's get 100% code coverage!", () => {
-  expect(true).toBe(false);
-});
+test("let's get 100% code coverage!", () => {expect(true).toBe(false)});
