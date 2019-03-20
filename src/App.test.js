5842b5754bcd9b82586766647d59e2b253d9cfba
Rohan Bassett <rohan@24digital.com.au>
2019-03-20 16:13:01 +1100

    tests pass

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("renders without crashing", () => {const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<App />, div); ReactDOM.unmountComponentAtNode(div);
 });
 
 test("falsey mcfalseface", () => {
 // ch-ch-ch-changes...
 expect(false).toBe(false)});
-test("let's get 100% code coverage!", () => {expect(true).toBe(false)});
+test("let's get 100% code coverage!", () => {expect(true).toBe(true)});
