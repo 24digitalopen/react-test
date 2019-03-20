0056f00dc1331560ce298b3a37d74f1fb719e907
Rohan Bassett <rohan@24digital.com.au>
2019-03-20 17:01:13 +1100

    node eslint.js

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -1,7 +1,7 @@
 for file in $(git show --name-only | egrep '[.]js$');
 do
     echo $file
-	eslint $file --max-warnings 0
+	node ./node_modules/eslint/bin/eslint.js $file --max-warnings 0
 	# If the result of the previous command is NOT zero
 	if [ $? -ne 0 ]; then
 	 echo "*** ERROR *** : eslint failed for file: $file, please fix code and recommit"
