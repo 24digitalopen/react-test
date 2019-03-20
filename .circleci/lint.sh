6c0bf3667475997f5e1389e52d8db43b55abf8be
Rohan Bassett <rohan@24digital.com.au>
2019-03-20 17:29:06 +1100

    fix to variable declaration

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -1,4 +1,4 @@
-$code=0
+code=0
 
 for file in $(find ./src -name *.js);
 do
@@ -6,7 +6,7 @@ do
 	node ./node_modules/eslint/bin/eslint.js $file --max-warnings 0
 	# If the result of the previous command is NOT zero
 	if [ $? -ne 0 ]; then
-	 $code=1
+	 code=1
 	fi
 done
 
