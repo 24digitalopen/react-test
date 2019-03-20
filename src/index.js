fa07b6b0e37be291e5e816d67e9815a2cd31a583
Rohan Bassett <rohan@24digital.com.au>
2019-03-20 17:01:58 +1100

    change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {Component, useRef,useState} from 'react';
+import React, {Component, useRef,} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
