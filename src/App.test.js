79f69d8503179f9c235f3a5fbc568c6083be26e5
24rohan <43631846+24rohan@users.noreply.github.com>
2019-03-20 16:19:16 +1100

    &c. (#7)
    * failing test
    
    * Squashed commit of the following:
    
    commit 635d677e57bd0560cdb4afa726ccf93b86d90648
    Author: Rohan Bassett <rohan@24digital.com.au>
    Date:   Wed Mar 20 15:28:34 2019 +1100
    
        prettify test
    
    commit 3a30e57aabfcbc85040b426c060ab4394b5823ab
    Author: michael24digital <42761843+michael24digital@users.noreply.github.com>
    Date:   Wed Mar 20 14:31:14 2019 +1100
    
        failing test (#5)
    
    commit f61ea814f76caa9e1b1657753d17f14ba0729e0e
    Merge: 6789b65 a763b53
    Author: 24rohan <43631846+24rohan@users.noreply.github.com>
    Date:   Wed Mar 20 14:10:58 2019 +1100
    
        Merge pull request #4 from 24digitalopen/feature/test
    
        truthy check
    
    commit 6789b65a959208cc629d552b407873cf642a24a1
    Merge: 04af1ab 2eff049
    Author: 24rohan <43631846+24rohan@users.noreply.github.com>
    Date:   Wed Mar 20 14:06:02 2019 +1100
    
        Merge pull request #3 from 24digitalopen/feature/test
    
        passing test
    
    commit 04af1ab120aeda4624b88b7b5ad5c2032cc037cf
    Merge: 3a350d3 e092601
    Author: 24rohan <43631846+24rohan@users.noreply.github.com>
    Date:   Wed Mar 20 13:54:14 2019 +1100
    
        Merge pull request #1 from 24digitalopen/feature/test
    
        #yolo
    
    * unstyle
    
    * test
    
    * circleci update
    
    * further
    
    * &c.
    
    * blahhh
    
    * argle
    
    * js file to test
    
    * prettier.sh
    
    * script changes
    
    * prettify all JS
    
    * tests pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,12 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {const div = document.createElement("div");
+  ReactDOM.render(<App />, div); ReactDOM.unmountComponentAtNode(div);
 });
 
 test("falsey mcfalseface", () => {
-  // ch-ch-ch-changes
-  expect(false).toBe(false);
-});
-
-test("let's get 100% code coverage!", () => {
-  expect(true).toBe(false);
-});
+// ch-ch-ch-changes...
+expect(false).toBe(false)});
+test("let's get 100% code coverage!", () => {expect(true).toBe(true)});
